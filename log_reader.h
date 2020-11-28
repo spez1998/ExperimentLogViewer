0c0fe71c85ae5a07108861072f028b7a79c7893a
spez1998 <samman30@live.com>
2020-11-28 20:08:09 +0000

    Added includes for new code

--- log_reader.h
+++ log_reader.h
@@ -2,11 +2,17 @@
 
 #include <ctype.h>
 #include <dirent.h>
+#include <errno.h>
+#include <getopt.h>
+#include <libgen.h>
 #include <stdbool.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 #include <time.h>
+#include <unistd.h>
 
 #ifdef _WIN32
 const char * strp_weekdays[] = 
